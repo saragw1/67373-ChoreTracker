5ea264ba73370ea9270dbf708ab8d7160c50f846
Sara Wong <swong1@andrew.cmu.edu>
2021-02-06 14:25:25 +0800

    Added validations and scopes to chore model

--- app/models/chore.rb
+++ app/models/chore.rb
@@ -2,5 +2,14 @@ class Chore < ApplicationRecord
 
     belongs_to :child
     belongs_to :task
+
+    validates_date :due_on
+
+    scope :by_task, -> { joins(:task).order("name") }
+    scope :chronological, -> { order(:due_on, :completed)}
+    scope :pending, -> { where(completed: false)}
+    scope :done, -> { where(completed: true) }
+    scope :upcoming, -> { where("due_on >= ?", Date.current)}
+    scope :past, -> { where("due_on < ?", Date.current)}
     
 end
