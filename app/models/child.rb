3c939fab3ef10cbb0607e310f757f5120b497263
Sara Wong <swong1@andrew.cmu.edu>
2021-02-06 12:51:55 +0800

    Added points_earned method to child model

--- app/models/child.rb
+++ app/models/child.rb
@@ -5,11 +5,16 @@ class Child < ApplicationRecord
 
   validates_presence_of :first_name, :last_name
 
+
+  scope :alphabetical, -> { order("last_name, first_name") }
+  scope :active, -> { where(active: true) }
+
   def name
     first_name + " " + last_name
   end
 
-  scope :alphabetical, -> { order("last_name, first_name") }
-  scope :active, -> { where(active: true) }
+  def points_earned
+    self.chores.done.inject(0){|sum,chore| sum += chore.task.points}
+  end 
 
 end
